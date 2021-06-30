1784224a107d8a4959d134eddd6027e749de129f
tejas-trivedi <500077185@stu.upes.ac.in>
2021-06-30 23:30:26 +0530

    cart view

--- cart/api/views.py
+++ cart/api/views.py
@@ -1,31 +1,65 @@
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.shortcuts import get_list_or_404
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics, mixins, serializers, status
+from rest_framework.generics import CreateAPIView
+from django.shortcuts import get_object_or_404
+
+
+from .serializers import *
+from users.models import User
+from cart.models import *
+
+
+class MyCartListView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request,*args,**kwargs):
+        try:
+            cart = Cart.objects.filter(user=request.user)
+            context = {
+                "request": request,
+            }
+
+            my_cart_serializer = CartSerializer(cart, many=True, context=context)
+            response = my_cart_serializer.data
+            return Response(response, status=status.HTTP_200_OK)
+
+        except Cart.DoesNotExist:
+            errors = {"message":["No cart found"]}
+            return Response(errors, status=status.HTTP_404_NOT_FOUND)
 
 
 
-class CartSerializer(serializers.ModelSerializer):
-    product = serializers.SerializerMethodField()
+class CartItemsListView(APIView):
 
-    class Meta:
-        model = Cart
-        fields = '__all__'
+    permission_classes = [IsAuthenticated]
 
-    @staticmethod
-    def get_product(obj):
-        return obj.product.name
+    def get(self,request,*args,**kwargs):
+        try:
+            #cart1 = Cart.objects.filter(user=request.user)
+            cart1 = get_object_or_404(Cart, user=request.user)
+            print(cart1)
+            cart_item = CartItem.objects.filter(cart=cart1.items.all())
+            context = {
+                "request": request,
+            }
 
+            cart_item_serializer = CartItemSerializer(cart_item, many=True, context=context)
+            response = cart_item_serializer.data
+            return Response(response, status=status.HTTP_200_OK)
 
-class CartItemSerializer(serializers.ModelSerializer):
-    item = serializers.SerializerMethodField()
-    item_title = serializers.SerializerMethodField()
-    product = serializers.SerializerMethodField()
-    price = serializers.SerializerMethodField()
+        except Cart.DoesNotExist:
+            errors = {"message":["No cart found"]}
+            return Response(errors, status=status.HTTP_404_NOT_FOUND)
 
-    class Meta:
-        model = CartItem
-        fields = [
-            "item",
-            "item_title",
-            "price",
-            "product",
-            "quantity",
-            "line_item_total",
-        ]

1cd1d39c2412ec792263ac2a9c86db03d5572171
tejas-trivedi <500077185@stu.upes.ac.in>
2021-07-08 13:21:17 +0530

    resolved migration | deleted entire db

--- users/api/urls.py
+++ users/api/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('wishlist/add/', AddCourseToWishlist.as_view(), name="add_to_wishlist"),
+    #path('wishlist/add/', AddCourseToWishlist.as_view(), name="add_to_wishlist"),
 ]
 
--- users/api/views.py
+++ users/api/views.py
@@ -29,19 +29,19 @@ class AddCourseToWishlist(APIView):
 
     def post(self, request, *args, **kwargs):
 
-        #wish = Wishlist.objects.filter(user=request.user)
+        wish = Wishlist.objects.filter(user=request.user)
 
-        course_id = request.data.get("course_id")
+        course_id = list(request.data.get("course_id"))
 
-        wishlist_course = AllCourses.objects.filter(id=course_id)
-        print(wishlist_course[0])
+        #wishlist_course = list(AllCourses.objects.filter(id=course_id))
+        #print(wishlist_course)
 
         data = {
             "user": request.user,
-            "wishlist": wishlist_course[0],
+            "wishlist": course_id,
         }
 
-        serializer = WishlistSerializer(data=data, partial=True)
+        serializer = WishlistSerializer(wish[0], data=data, partial=True)
         if serializer.is_valid():
             serializer.save()
             response = {
@@ -57,3 +57,118 @@ class AddCourseToWishlist(APIView):
                 {"message": serializer.errors},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+
+"""
+class CreateMyCart(APIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        #items = request.data.get("items")
+
+        data = {
+            "user": request.user,
+            #"items": items,
+        }
+        serializer = CartSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "Your cart is ready"
+            }
+            return Response(
+                response,
+                status = status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"message": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class UpdateMyCart(APIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+
+        cart = Cart.objects.filter(user=request.user)
+        context = {
+                "request": request,
+            }
+        cart_item = CartItem.objects.filter(cart=cart[0])
+
+        #my_cart = Cart()
+        total_cart_amount = 0
+        cart_item_serializer = CartItemSerializer(cart_item, many=True, context=context)
+        response_temp = cart_item_serializer.data
+
+        for res in range(0, len(response_temp)):
+            total_cart_amount += Decimal(response_temp[res]['line_item_total'])
+
+        print(total_cart_amount)
+
+        data = {
+            "total": total_cart_amount,
+        }
+        serializer = CartSerializer(cart[0], data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "Your cart is updated"
+            }
+            return Response(
+                response,
+                status = status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"message": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+
+class AddItemToCart(APIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+
+        my_cart = Cart.objects.filter(user=request.user)
+
+        cart = my_cart[0]
+        #print(cart)
+        item = request.data.get("item")
+        quantity = request.data.get("quantity")
+
+        item_course = AllCourses.objects.filter(id=item)
+        #item_price = item_course.discounted_price
+        item_price = Decimal(list(item_course.values_list('discounted_price', flat=True))[0])
+        print(item_price)
+
+        data = {
+            "cart": str(cart),
+            "item": item,
+            "quantity": quantity,
+            "line_item_total": item_price*int(quantity),
+        }
+
+        serializer = CartItemSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "Item has been added to the cart"
+            }
+
+            return Response(
+                response,
+                status = status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"message": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )"""
